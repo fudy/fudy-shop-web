2c6dcff78f9adff7ee428a8324c49c9320a9121f
Fudy <lifudong1@163.com>
2022-06-19 14:04:38 +0800

    login session

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { API_BASE_URL } from './main';
+axios.defaults.withCredentials = true;
 
 export const registerUser = function (params, successHandler, errorHandler) {
     axios.post(API_BASE_URL+"users",params).then(res=>{
@@ -27,6 +28,14 @@ export const smsLogin = function (params, successHandler, errorHandler) {
 
 export const modifyPassword = function (params, successHandler, errorHandler) {
     axios.post(API_BASE_URL+"user/password", params).then(res=>{
+        successHandler(res.data);
+    },err=>{
+        errorHandler(err.message);
+    })
+}
+
+export const getLoginUser = function (successHandler, errorHandler) {
+    axios.get(API_BASE_URL+"user").then(res=>{
         successHandler(res.data);
     },err=>{
         errorHandler(err.message);
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,30 @@
 <template>
   <a-space>
-    <router-link to="/login">登录</router-link>
-    <router-link to="/registry">注册</router-link>
+    <template v-if="user.userName !== null">
+        <span>欢迎 {{user.userName}}</span>
+    </template>
+    <template v-else>
+        <router-link to="/login">登录</router-link>
+        <router-link to="/registry">注册</router-link>
+    </template>
   </a-space>
 </template>
+<script setup>
+  import { onMounted,reactive,ref} from 'vue';
+  import { getLoginUser } from '../api/user';
+  let user = reactive({});
+  onMounted(() => {
+    getLoginUser((res)=> {
+        if(res.success) {
+            Object.assign(user, res.data)
+        }
+    }, (err)=> {
+        console.log(err);
+    });
+  })
+
+
+</script>
+
 
 
