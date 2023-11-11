9ccf9814ed3e49200742d7d32adbc89fc23944de
Fudy <lifudong1@163.com>
2023-11-11 22:42:22 +0800

    pinia persist

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate' 
 import App from "./App.vue";
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import router from './router/index.js'
 
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate); //使用pinia持久化插件
 
 const app = createApp(App)
     .use(pinia)
--- src/stores/user.js
+++ src/stores/user.js
@@ -6,5 +6,6 @@ export const useUserStore = defineStore('user', {
         userName: null,
         avatar: null
     }
-  }
+  },
+  persist:true
 })
