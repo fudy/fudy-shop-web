5ac325efc42b7898b4738335af50e140715b372e
Fudy <lifudong1@163.com>
2024-03-10 11:51:28 +0800

    modify login css

--- src/views/login.vue
+++ src/views/login.vue
@@ -7,8 +7,8 @@ const activeKey= ref('1');
 
 <template>
     <div class="bk"> 
-        <div style="float:right;margin:150px 200px;background-color:#fff;width: 400px;border-radius: 5px;">
-            <a-tabs v-model:activeKey="activeKey" style="text-align: 'left'">
+        <div style="float:right;margin:150px 200px;background-color:#fff;width: 400px;border-radius: 5px;padding:20px">
+            <a-tabs v-model:activeKey="activeKey" >
                 <a-tab-pane key="1" tab="密码登录"> <Login /></a-tab-pane>
                 <a-tab-pane key="2" tab="短信登录" force-render><SmsCaptchaLogin /></a-tab-pane> 
             </a-tabs>
