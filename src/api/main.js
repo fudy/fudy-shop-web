43fe0ce090d05e6e1a17ba275774b55fe510a114
Fudy <lifudong1@163.com>
2024-03-10 11:18:57 +0800

    save

--- src/api/main.js
+++ src/api/main.js
@@ -3,4 +3,4 @@ export const API_HOMEPAGE_BASE_URL="/api/fudy-shop-homepage/";
 //核心应用fudy-shop的服务
 export const API_BASE_URL="/api/fudy-shop/";
 //商品详情应用fudy-shop-item-detail的服务
-export const API_ITEM_DETAIL_BASE_URL="/api/fudy-shop-item-detail";
+export const API_ITEM_DETAIL_BASE_URL="/api/fudy-shop-item-detail/";
